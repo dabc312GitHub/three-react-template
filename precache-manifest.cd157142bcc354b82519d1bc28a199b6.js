bc9be4f34ef9d82985e39bd30eb93328046898a1
Etienne Monget <etienne.monget@pm.me>
2020-05-12 18:47:58 +0200

    Updates

---
+++ precache-manifest.cd157142bcc354b82519d1bc28a199b6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3453d4f5440ecd69ce91c5b3600bab8c",
+    "url": "/three-react-template/index.html"
+  },
+  {
+    "revision": "f639e21a0208c526e1d2",
+    "url": "/three-react-template/static/css/10.dac45d56.chunk.css"
+  },
+  {
+    "revision": "3ea56463e5cd10a466ab",
+    "url": "/three-react-template/static/css/11.d759a15a.chunk.css"
+  },
+  {
+    "revision": "b449f66521e54a86f8d7",
+    "url": "/three-react-template/static/css/2.d759a15a.chunk.css"
+  },
+  {
+    "revision": "2bdb86317cb2dd42a8ba",
+    "url": "/three-react-template/static/css/7.d759a15a.chunk.css"
+  },
+  {
+    "revision": "1e754e16da0035ad5b22",
+    "url": "/three-react-template/static/css/8.d759a15a.chunk.css"
+  },
+  {
+    "revision": "523d4c0a6700ebb7eb70",
+    "url": "/three-react-template/static/css/9.d759a15a.chunk.css"
+  },
+  {
+    "revision": "83197e7eb276577c5521",
+    "url": "/three-react-template/static/css/main.e8afb8f9.chunk.css"
+  },
+  {
+    "revision": "9a64a60b28966f7b4a00",
+    "url": "/three-react-template/static/js/0.2e53eb4f.chunk.js"
+  },
+  {
+    "revision": "0ca1680f3877b68ff190",
+    "url": "/three-react-template/static/js/1.43fef891.chunk.js"
+  },
+  {
+    "revision": "6b46877d8286bef602759ceceb6e67db",
+    "url": "/three-react-template/static/js/1.43fef891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f639e21a0208c526e1d2",
+    "url": "/three-react-template/static/js/10.56c823a0.chunk.js"
+  },
+  {
+    "revision": "3ea56463e5cd10a466ab",
+    "url": "/three-react-template/static/js/11.059c462f.chunk.js"
+  },
+  {
+    "revision": "3059fa78d4a20cd1920d",
+    "url": "/three-react-template/static/js/12.b42cf776.chunk.js"
+  },
+  {
+    "revision": "b449f66521e54a86f8d7",
+    "url": "/three-react-template/static/js/2.6b9957eb.chunk.js"
+  },
+  {
+    "revision": "8125fa67eb633c8e6daa",
+    "url": "/three-react-template/static/js/3.1da31b23.chunk.js"
+  },
+  {
+    "revision": "826ba060514d1977c0c3",
+    "url": "/three-react-template/static/js/6.0fa9ee2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/three-react-template/static/js/6.0fa9ee2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bdb86317cb2dd42a8ba",
+    "url": "/three-react-template/static/js/7.a4358adb.chunk.js"
+  },
+  {
+    "revision": "1e754e16da0035ad5b22",
+    "url": "/three-react-template/static/js/8.d5ea3271.chunk.js"
+  },
+  {
+    "revision": "523d4c0a6700ebb7eb70",
+    "url": "/three-react-template/static/js/9.208927bc.chunk.js"
+  },
+  {
+    "revision": "83197e7eb276577c5521",
+    "url": "/three-react-template/static/js/main.0ec0d2d0.chunk.js"
+  },
+  {
+    "revision": "05c72b8afb13d6927b1d",
+    "url": "/three-react-template/static/js/runtime-main.eb49aca1.js"
+  },
+  {
+    "revision": "f3152ab191c5a7dedad016c9b16308f3",
+    "url": "/three-react-template/static/media/Astronaut.f3152ab1.glf"
+  },
+  {
+    "revision": "ebf4afeb87874d0a1487e7ea687fb529",
+    "url": "/three-react-template/static/media/Horse.ebf4afeb.glb"
+  },
+  {
+    "revision": "be1ebb921ea1610aad9b0488b42dbdff",
+    "url": "/three-react-template/static/media/sand.be1ebb92.jpg"
+  },
+  {
+    "revision": "3d153e6a36a8f7d4e762a8a1b89fd7ac",
+    "url": "/three-react-template/static/media/sand_norm.3d153e6a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/three-react-template/precache-manifest.d31f5381779e03348169b411ae816794.js"
+  "/three-react-template/precache-manifest.cd157142bcc354b82519d1bc28a199b6.js"
 );
 
 self.addEventListener('message', (event) => {
